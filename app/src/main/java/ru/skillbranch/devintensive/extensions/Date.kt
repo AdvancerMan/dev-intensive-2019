cfe5e4c70973ccd8b677aaadbb55346057cb359d
Kazakov Mihail <advancerman@gmail.com>
2020-08-15 22:22:35 +0700

    Implemented Date.add extension function

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -6,4 +6,11 @@ import java.util.*
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy") =
     SimpleDateFormat(pattern, Locale("ru")).format(this)
 
+enum class TimeUnits(val toMilliseconds: Long) {
+    SECOND(1000), MINUTE(SECOND.toMilliseconds * 60),
+    HOUR(MINUTE.toMilliseconds * 60), DAY(HOUR.toMilliseconds * 24);
+}
 
+fun Date.add(value: Long, units: TimeUnits) {
+    time += value * units.toMilliseconds
+}
