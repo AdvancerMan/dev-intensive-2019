e93363c4aca512acf8409cd3ec6a34023903dd41
Kazakov Mihail <advancerman@gmail.com>
2020-08-15 22:59:29 +0700

    Implemented Builder pattern for User class

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -20,4 +20,25 @@ data class User(
             return User("${maxId++}", firstName, lastName, null)
         }
     }
+
+    class Builder {
+        lateinit var id: String
+        var firstName: String? = null
+        var lastName: String? = null
+        var avatar: String? = null
+        var rating: Int = 0
+        var respect: Int = 0
+        var lastVisit: Date? = Date()
+        var isOnline: Boolean = false
+
+        fun id(id: String) { this.id = id }
+        fun firstName(firstName: String?) { this.firstName = firstName }
+        fun lastName(lastName: String?) { this.lastName = lastName }
+        fun avatar(avatar: String?) { this.avatar = avatar }
+        fun rating(rating: Int) { this.rating = rating }
+        fun respect(respect: Int) { this.respect = respect }
+        fun lastVisit(lastVisit: Date?) { this.lastVisit = lastVisit }
+        fun isOnline(isOnline: Boolean) { this.isOnline = isOnline }
+        fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
+    }
 }
