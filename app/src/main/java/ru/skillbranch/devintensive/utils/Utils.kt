34a5304e71a0e3950a7b17c4d848d06cebf7e63f
Kazakov Mihail <advancerman@gmail.com>
2020-08-15 22:28:57 +0700

    Implemented Utils.toInitials function

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,10 +2,17 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     private val whitespaces = Regex("""(\\s)*""")
+
     fun parseFullName(fullName: String?) =
         fullName
             ?.split(whitespaces)
             ?.filterNot { it.isBlank() }
             ?.let { it.getOrNull(0) to it.getOrNull(1) }
             ?: null to null
+
+    private fun toInitial(name: String?) =
+        name?.trimStart()?.firstOrNull()?.toUpperCase()?.toString() ?: ""
+
+    fun toInitials(firstName: String?, lastName: String?) =
+        (toInitial(firstName) + toInitial(lastName)).ifEmpty { null }
 }
